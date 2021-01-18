cc7c5a1f230bad44a24e84385bb885bcb2669bbd
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2021-01-17 22:59:05 -0600

    add dependencies & insert sample code into main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use simple_error::bail;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Deserialize, Clone)]
+struct CustomEvent {
+    first_name: String,
+    last_name: String,
 }
+
+#[derive(Serialize, Clone)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() {
+    lambda!(my_handler);
+}
+
+fn my_handler(e: CustomEvent, _ctx: Context) -> Result<CustomOutput, HandlerError> {
+    if e.first_name == "" {
+        bail!("Empty first name");
+    }
+    Ok(CustomOutput{
+        message: format!("Hello, {}!", e.first_name),
+    })
+}
+
